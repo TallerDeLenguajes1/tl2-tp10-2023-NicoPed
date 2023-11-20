1f4699d08b2867e1fac9e8184c9d0a923c55e5f5
NicoPed <nikopedraza10@gmail.com>
2023-11-20 20:28:49 -0300

    Cambiado la BD y agregado campos a usuario

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,13 +1,23 @@
 namespace tl2_tp10_2023_NicoPed;
 
+public enum Rol{
+    Administrador,
+    Operador
+}
 public class Usuario{
     private int id_usuario;
     private string? nombre_de_usuario;
+    private string? contrasenia;
 
+    private Rol rolDeUsuario;
     public Usuario()
     {
     }
 
     public int Id_usuario { get => id_usuario; set => id_usuario = value; }
     public string? Nombre_de_usuario { get => nombre_de_usuario; set => nombre_de_usuario = value; }
+    public string? Contrasenia { get => contrasenia; set => contrasenia = value; }
+    public Rol RolDeUsuario { get => rolDeUsuario; set => rolDeUsuario = value; }
+
+
 }
